985c7fdc2b33958e2d4ac71f77432e2ffd9bcbbb
pegga415 <77526388+pegga415@users.noreply.github.com>
2023-02-22 16:58:49 +0800

    Add files via upload

---
+++ st.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    var myclass=document.getElementById("myclass");
+    if(myclass.style.display=="none"){
+    	myclass.style.display="block";
+    }else{
+    	myclass.style.display="none";
+ 	}
+ }
